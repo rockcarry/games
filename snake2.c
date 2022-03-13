5d8d1ff681348454ac1a8d3493d37d8aebe9c3b5
chenk <chenk@apical.com.cn>
2022-03-13 15:29:00 +0800

    修改 snake2.c 代码，解决 win 判断错误的问题

--- snake2.c
+++ snake2.c
@@ -4,8 +4,8 @@
 int main(void) {
     int map[19 * 15] = {0}, head = 19 * 15 / 2 - 1, newh = head + 1, die = 0, eat, key, i, n; // define variables
     for (map[newh] = -1; !die && system("cls") == 0; ) {
-        if (eat = map[newh] == -1, map[newh] = map[head] + 1, head = newh, map[head] == 19 * 15) break; // eat food and check win
-        for (i = 0, n = rand() % (19 * 15 - map[head]); eat && i < 19 * 15; i++) { if (!map[i] && --n == -1) { map[i] = -1; break; } } // new food
+        if (eat = map[newh] == -1, map[newh] = map[head] + 1, head = newh, map[head] == 19 * 15 + 1) break; // eat food and check win
+        for (i = 0, n = rand() % (19 * 15 + 1 - map[head]); eat && i < 19 * 15; i++) { if (!map[i] && --n == 0) { map[i] = -1; break; } } // new food
         for (i = 0; i < 19 * 15; i++) { // draw game
             map[i] -= !eat && map[i] > 0;
             printf("%c%c", i == head ? '@' : map[i] == -1 ? '$' : map[i] != 0 ? '*' : '.', i % 19 == 19 - 1 ? '\n' : ' ');
