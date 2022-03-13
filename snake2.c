8e97da0732b72593504597919ede283c239330e8
chenk <chenk@apical.com.cn>
2022-03-13 16:39:10 +0800

    解决投食的 bug，解决 _getch 编译警告

--- snake2.c
+++ snake2.c
@@ -5,7 +5,7 @@ int main(void) {
     int map[19 * 15] = {0}, head = 19 * 15 / 2 - 1, newh = head + 1, die = 0, eat, key, i, n; // define variables
     for (map[newh] = -1; !die && system("cls") == 0; ) {
         if (eat = map[newh] == -1, map[newh] = map[head] + 1, head = newh, map[head] == 19 * 15 + 1) break; // eat food and check win
-        for (i = 0, n = rand() % (19 * 15 + 1 - map[head]); eat && i < 19 * 15; i++) { if (!map[i] && --n == 0) { map[i] = -1; break; } } // new food
+        for (i = 0, n = rand() % (19 * 15 + 1 - map[head]); eat && i < 19 * 15; i++) { if (!map[i] && --n <= 0) { map[i] = -1; break; } } // new food
         for (i = 0; i < 19 * 15; i++) { // draw game
             map[i] -= !eat && map[i] > 0;
             printf("%c%c", i == head ? '@' : map[i] == -1 ? '$' : map[i] != 0 ? '*' : '.', i % 19 == 19 - 1 ? '\n' : ' ');
@@ -16,5 +16,5 @@ int main(void) {
         if ( key             == 'F' || key == 'f') newh = head + 1;
         die = newh < 0 || newh >= 19 * 15 || (abs(head - newh) == 1 && head / 19 != newh / 19) || (map[newh] && map[newh] != 1 && map[newh] != -1); // check die
     }
-    printf("\nyou %s !\n", map[head] == 19 * 15 ? "win" : "lost"); _getch(); return 0;
+    printf("\nyou %s !\n", map[head] == 19 * 15 ? "win" : "lost"); (void)_getch(); return 0;
 }
